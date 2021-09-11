01481f2bd3a8738911bf093c24dd09ca120138c8
JustinLoo <36491619+JustinLoo@users.noreply.github.com>
2021-09-11 15:42:15 -0700

    forth commit

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
+  // Load Libraries
+  require_once 'libraries/core.php';
+  require_once 'libraries/controller.php';
+  require_once 'libraries/database.php';
--- app/libraries/core.php
+++ app/libraries/core.php
@@ -1,24 +1,59 @@
 <?php
-
-class Core{
+  /*
+   * App Core Class
+   * Creates URL & loads core controller
+   * URL FORMAT - /controller/method/params
+   */
+  class Core {
     protected $currentController = 'Pages';
-
     protected $currentMethod = 'index';
-
     protected $params = [];
 
-
     public function __construct(){
-        $this->getUrl();
+      //print_r($this->getUrl());
+
+      $url = $this->getUrl();
+
+      // Look in controllers for first value
+      if(file_exists('../app/controllers/' . ucwords($url[0]). '.php')){
+        // If exists, set as controller
+        $this->currentController = ucwords($url[0]);
+        // Unset 0 Index
+        unset($url[0]);
+      }
+
+      // Require the controller
+      require_once '../app/controllers/'. $this->currentController . '.php';
+
+      // Instantiate controller class
+      $this->currentController = new $this->currentController;
+
+
+      // check for second parameter in url
+
+      if(isset($url[1])){
+
+        if(method_exists($this->currentController, $url[1])){
+
+          $this->currentMethod = $url[1];
+
+
+        }
+
+
+      }
+
+      echo $this->currentMethod;
     }
 
     public function getUrl(){
-
-        echo $_GET['url'];
-
-      
+      if(isset($_GET['url'])){
+        $url = rtrim($_GET['url'], '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+        return $url;
+      }
     }
-
-
-
-}
+  } 
+  
+  
